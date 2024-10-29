6d3e8e5a466506d69b432d057e362d2cbbfd2fc6
Dũng Quang <22028229@vnu.edu.vn>
2024-10-29 20:57:10 +0700

    Setup merge to main

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,5 +2,5 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 import './index.css'
-import ExploreRecipe from'./pages/ExploreRecipes'
-ReactDOM.createRoot(document.getElementById('root')).render(<ExploreRecipe />)
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
