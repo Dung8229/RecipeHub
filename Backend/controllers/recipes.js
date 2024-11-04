0f78adde0893c6ce23859a218eea4172098d4e58
Phạm Việt Hồng <phamviethong672003@gmail.com>
2024-11-05 05:18:03 +0700

    Update recipes.js

--- Backend/controllers/recipes.js
+++ Backend/controllers/recipes.js
@@ -1,26 +1,10 @@
 // api liên quan công thức
 const recipesRouter = require('express').Router()
 const Recipe = require('../models/recipe')
-const RecipeRating = require('../models/recipeRating')
-const RecipeComment = require('../models/recipeComment')
+const RecipeRating = require('../models/recipe_rating')
+const RecipeComment = require('../models/recipe_comment')
 const RecipeAverageRating = require('../models/recipe_averagerating')
 const middleware = require('../utils/middleware')
-
-// Lấy các công thức dinner
-// const getDinnerRecipes = async (req, res) => {
-//     try {
-//       const dinnerRecipes = await Recipe.findAll({
-//         where: { category: 'dinner' }
-//       });
-//       res.json(dinnerRecipes);
-//     } catch (error) {
-//       console.error('Error fetching dinner recipes:', error);
-//       res.status(500).json({ message: 'Server Error' });
-//     }
-//   };
-
-// recipesRouter.get('/dinner', getDinnerRecipes);
-
 
 // GET /api/recipes/:recipeId - Lấy thông tin công thức, đánh giá và bình luận
 recipesRouter.get('/:recipeId', async (req, res) => {
